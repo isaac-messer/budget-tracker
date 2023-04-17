6870b9a9ca0d8b94631675e5706f30e5edf33983
Isaac Messer <isaac.messer640.com>
2023-04-16 21:58:34 -0600

    Removed render function due to confusion

--- budget-tracker/src/Reducer.js
+++ budget-tracker/src/Reducer.js
@@ -1,10 +1,15 @@
-import {useReducer} from 'react';
+// import {useReducer} from 'react';
 
-const BudgetReducer = (state, action) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
-};
+// const BudgetReducer = (state, action) => {
+//     switch (action.type) {
+//         case 'ADD_TRANSACTION':
+//             return {
+//                 ...state,
+//                 transactions: [...state.transactions, action.payload],
+//             };
+//         default:
+//             return state;
+//     }
+// };
 
-export default BudgetReducer;
+// export default BudgetReducer;
