6ef6b256659581d5e861a70de8fbd724832123b1
Isaac Messer <isaac.messer640.com>
2023-04-16 21:57:59 -0600

    Moved render function into Context.js for testing

--- budget-tracker/src/Context.js
+++ budget-tracker/src/Context.js
@@ -1,9 +1,27 @@
 import {createContext, useReducer, } from 'react';
-import BudgetReducer from './Reducer.js'
+// import BudgetReducer from './Reducer.js'
+
+const BudgetReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [...state.transactions, action.payload],
+            };
+        default:
+            return state;
+    }
+};
+
+
 
 const initialState = {
-    budge: null,
-    transactions: []
+    income: 1500,
+    transactions: [
+        {category: 'Food', amount: 20, id: crypto.randomUUID()},
+        {category: 'Gas', amount: 50, id: crypto.randomUUID()},
+        {category: 'Store', amount: 130, id: crypto.randomUUID()},
+    ]
 };
 
 export const BudgetContext = createContext();
@@ -14,9 +32,15 @@ export const BudgetProvider = (prop) => {
     return (
         <BudgetContext.Provider
             value={{
+                transactions: state.transactions,
+
                 balance: state.balance,
                 income: state.income,
-                expenses: state.expenses,
+                expense: state.expense,
+
+                category: state.category,
+                amount: state.amount,
+
                 dispatch,
             }}
         >   {prop.children}    
