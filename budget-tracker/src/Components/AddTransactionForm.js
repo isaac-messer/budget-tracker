bf71d83056297adca63c2b8cbc65fb6240f6db40
Isaac Messer <isaac.messer640.com>
2023-04-16 21:56:42 -0600

    Added ability to render new transaction items

--- budget-tracker/src/Components/AddTransactionForm.js
+++ budget-tracker/src/Components/AddTransactionForm.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { BudgetContext } from "../Context";
+import { v4 as uuidv4 } from 'uuid';
 
 const AddTransactionForm = () => {
+    const { dispatch } = useContext(BudgetContext);
+
+    const [category, setCategory] = useState('');
+    const [amount, setAmount] = useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const transaction = {
+            category: category,
+            amount: parseInt(amount),
+            id: uuidv4(),
+        };
+
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction,
+        });
+
+    };
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div id='transactionCatagoryInput'>
                 <label htmlFor='category'>Category</label>
                 <input 
                     required='required'
                     type='text' 
                     id='name'
+                    value={category}
+                    onChange={(event) => setCategory(event.target.value)}
                 />
             </div>
             <div id="transactionValueInput">
@@ -17,6 +42,8 @@ const AddTransactionForm = () => {
                 required='required'
                 type='text'
                 id='value'
+                value={amount}
+                onChange={(event) => setAmount(event.target.value)}
                 />
             </div>
             <div id="transactionSubmitButton">
