179b10c5c8d86e3b8667c71a0328d8474b1fe2d7
Isaac Messer <isaac.messer640.com>
2023-04-16 20:59:39 -0600

    Created context file for global variable acess

--- budget-tracker/src/Context.js
+++ budget-tracker/src/Context.js
@@ -1,1 +1,25 @@
-import {createContext, } from 'react';
+import {createContext, useReducer, } from 'react';
+import BudgetReducer from './Reducer.js'
+
+const initialState = {
+    budge: null,
+    transactions: []
+};
+
+export const BudgetContext = createContext();
+
+export const BudgetProvider = (prop) => {
+    const [state, dispatch] = useReducer(BudgetReducer, initialState);
+
+    return (
+        <BudgetContext.Provider
+            value={{
+                balance: state.balance,
+                income: state.income,
+                expenses: state.expenses,
+                dispatch,
+            }}
+        >   {prop.children}    
+        </BudgetContext.Provider>
+    )
+}
