8ba958d39cc642b5014f2ae32e9fc9785202aaff
Isaac Messer <isaac.messer640.com>
2023-04-16 20:55:02 -0600

    Created reducer file/function to store state logic

--- budget-tracker/src/Reducer.js
+++ budget-tracker/src/Reducer.js
@@ -1,1 +1,10 @@
 import {useReducer} from 'react';
+
+const BudgetReducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default BudgetReducer;
