95d5781338db1a0609852f6fd86dbd13fe09aa48
Isaac Messer <isaac.messer640.com>
2023-04-16 18:59:12 -0600

    Imported Components

--- budget-tracker/src/App.js
+++ budget-tracker/src/App.js
@@ -1,29 +1,28 @@
-import './App.css';
-
 import { useEffect, useState, useRef } from 'react';
+import React from 'react';
+import './App.css';
+import Balance from './Components/BalanceInfo.js'
+import Income from './Components/IncomeInfo.js'
+import Expenses from './Components/ExpensesInfo';
+
+
+
 
 
 function App() {
   return (
-    <>
+    <div id="container">
       <div id="header">
-        <h1>Budget Tracker</h1>
       </div>
       <div id="budgetBalance">
-        <h2>Your Balance</h2>
-        <h2>$</h2>
         <div id="budgetBreakDown">
-          <h3>Income</h3>
-          <p>+$</p>
-          <h3>Expence</h3>
-          <p>-$</p>
         </div>
       </div>
       <div id="history">
       </div>
       <div id="addTransaction">
       </div>
-    </>
+    </div>
   );
 };
 
